9dcc2858991c73d6c8aadb01854ea0b12c9b9074
SirioRedGiant <Oiris1992@gmail.com>
2026-05-11 11:25:06 +0200

    Use Composition to associate the movie class with the genre class. Use of an "has- a" relationship

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+class Genre
+{
+    public string $type;
+
+    public function __construct(string $_type)
+    {
+        $this->type = $_type;
+    }
+}
+
 
 class Movie
 {
@@ -7,26 +17,43 @@ class Movie
     public string $title;
     public string $author;
     public int $year;
+    public array $genres;
 
 
-    public function __construct(string $_title, string $_author, int $_year)
+    public function __construct(string $_title, string $_author, int $_year, array $_genres)
     {
         $this->title = $_title;
         $this->author = $_author;
         $this->year = $_year;
+        $this->genres = $_genres;
+    }
+
+    public function getMovieGenresList(): string
+    {
+        $genreNames = [];
+        foreach ($this->genres as $genre) {
+            $genreNames[] = $genre->type;
+        }
+        return implode(", ", $genreNames);
     }
 
 
     public function getFullMovieDetails()
     {
-        return "Film: {$this->title}, Regia: {$this->author} ({$this->year})";
+        return "Film: {$this->title}, Regia: {$this->author} ({$this->year}) -> Generi: {$this->getMovieGenresList()}";
     }
 }
 
 
-$movie1 = new Movie("Inception", "Christopher Nolan", 2010);
-$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994);
-$movie3 = new Movie("Ace Ventura: Pet Detective", "Jack Bernstein", 1994);
+$action = new Genre("Azione");
+$sciFi = new Genre("Fantascienza");
+$comedy = new Genre("Commedia");
+$thriller = new Genre("Thriller");
+
+
+$movie1 = new Movie("Inception", "Christopher Nolan", 2010, [$action, $thriller, $sciFi]);
+$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, [$action, $thriller]);
+$movie3 = new Movie("Ace Ventura: Pet Detective", "Jack Bernstein", 1994, [$comedy]);
 
 
 echo $movie1->getFullMovieDetails() . "<br>";
